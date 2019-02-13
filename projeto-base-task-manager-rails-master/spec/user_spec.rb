b7276d7efff5c06634756bd5d295e7fe99d82a4a
Ricardo Ruy <ricardo.rruy@hotmail.com>
2019-02-13 11:30:49 -0200

    Install spring - IMproving rspec

--- projeto-base-task-manager-rails-master/spec/user_spec.rb
+++ projeto-base-task-manager-rails-master/spec/user_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  before { @user = FactoryGirl.build(:user) }
+#   before { @user = FactoryGirl.build(:user) }
 
-  it { expect(@user).to respond_to(:email) }
-  #it { expect(@user).to respond_to(:name) }
-  it { expect(@user).to respond_to(:password) }
-  it { expect(@user).to respond_to(:password_confirmation) }
-  it { expect(@user).to be_valid }
+#   it { expect(@user).to respond_to(:email) }
+#   #it { expect(@user).to respond_to(:name) }
+#   it { expect(@user).to respond_to(:password) }
+#   it { expect(@user).to respond_to(:password_confirmation) }
+#   it { expect(@user).to be_valid }
+
+  it { is_expected.to respond_to(:email) }
 end
