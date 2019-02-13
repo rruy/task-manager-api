fa145eb066653cef6e6beaeb118111aa93bfdf33
Ricardo Ruy <ricardo.rruy@hotmail.com>
2019-02-13 11:13:47 -0200

    Add Test model user

--- projeto-base-task-manager-rails-master/spec/factories/users.rb
+++ projeto-base-task-manager-rails-master/spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    
+    email "ricardo.rruy@hotmail.com" 
+    #name "Ricardo Ruy"
+    password "123456"
+    password_confirmation "123456"
   end
 end
