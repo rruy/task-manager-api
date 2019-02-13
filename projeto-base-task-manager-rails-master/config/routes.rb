894d8a385664cb62270b88907d62c208bd04565a
Ricardo Ruy <ricardo.rruy@hotmail.com>
2019-02-13 15:17:05 -0200

    Adding users controller

--- projeto-base-task-manager-rails-master/config/routes.rb
+++ projeto-base-task-manager-rails-master/config/routes.rb
@@ -7,12 +7,12 @@ Rails.application.routes.draw do
   #api.site.test/tasks
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: "/" do
     namespace :v1, path: "/", constraints: ApiVersionConstraint.new(version: 1) do
-      resources :tasks
+      resources :users, only: [:show]
     end
 
-    namespace :v2, path: "/", constraints: ApiVersionConstraint.new(version: 2, default: true) do
-      resources :tasks
-    end
+    # namespace :v2, path: "/", constraints: ApiVersionConstraint.new(version: 2, default: true) do
+    #   resources :tasks
+    # end
   end
 
 end
---
+++ projeto-base-task-manager-rails-master/spec/requests/api/v1/users_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe 'User API', type: :request do
+   let!(:user) { create(:user) }
+   let(:user_id) { user.id }
+
+   before { host! "api.taskmanager.test" }
+
+   describe "GET /users/:id" do
+     before do  
+        headers = { "Accept" => "application/vnd.taskmanager.v1" } 
+        get "/users/#{user_id}", {}, headers
+     end
+
+     context "when the user exists" do
+       it "returns the users" do
+         user_response = JSON.parse(response.body)   
+         expect(user_response["id"]).to eq(user_id)
+       end
+
+       it "returns status 200" do
+         expect(response).to have_http_status(200)
+       end
+     end
+
+     context "when the user does not exist" do
+       let(:user_id) { 10000 }
+
+       it "returns status 404" do 
+         expect(response).to have_http_status(404)
+       end
+     end
+
+   end
+    
+end
