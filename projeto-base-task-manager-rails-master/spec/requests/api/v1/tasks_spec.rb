db57f997a62591fc7cd549abb35e8bd020697283
Ricardo Ruy <ricardo.rruy@hotmail.com>
2019-02-28 14:57:55 -0300

    Include tests controller action index taks

---
+++ projeto-base-task-manager-rails-master/spec/requests/api/v1/tasks_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe 'Task API', type: :request do
+    before { host! 'api.taskmanager.test' }
+
+    let(:user) { create(:user) }
+    let(:headers) do
+      { 
+        'Accept' => 'application/vnd.taskmanager.v1',
+        'Content-type' => Mime[:json].to_s,
+        'Authorization' => user.auth_token
+      } 
+    end
+    
+    describe 'GET /tasks' do
+      before do
+        create_list(:task, 5, user_id: user.id) 
+        get '/tasks', params: {}, headers: headers
+      end
+
+      it 'return status code 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'return 5 tasks from database' do
+        expect(json_body[:tasks].count).to eq(5)
+      end
+
+    end
+end
