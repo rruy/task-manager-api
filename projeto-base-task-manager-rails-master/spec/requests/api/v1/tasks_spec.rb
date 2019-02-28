2f7593a7afe3291ddc98166cd571268cbd35c939
Ricardo Ruy <ricardo.rruy@hotmail.com>
2019-02-28 16:00:07 -0300

    Adding action destroy controller tasks

--- projeto-base-task-manager-rails-master/spec/requests/api/v1/tasks_spec.rb
+++ projeto-base-task-manager-rails-master/spec/requests/api/v1/tasks_spec.rb
@@ -133,5 +133,21 @@ RSpec.describe 'Task API', type: :request do
 
     end
 
+    describe 'DELETE /task/:id' do
+      let!(:task) { create(:task, user_id: user.id) }
+        
+      before do
+        delete "/tasks/#{task.id}", params: { }.to_json, headers: headers      
+      end
+
+      it 'returns status code 204' do
+        expect(response).to have_http_status(204)
+      end
+
+      it 'removes the task from the database' do
+        expect { Task.find(task.id) }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+
+    end
 
 end
