cff180c75c8062bd93423bdbd88561ed262af4df
Ricardo Ruy <ricardo.rruy@hotmail.com>
2019-03-04 11:53:09 -0300

    updating Session Api

--- projeto-base-task-manager-rails-master/spec/requests/api/v2/sessions_spec.rb
+++ projeto-base-task-manager-rails-master/spec/requests/api/v2/sessions_spec.rb
@@ -3,16 +3,20 @@ require 'rails_helper'
 RSpec.describe 'Session API', type: :request do
     before { host! 'api.taskmanager.test' }
     let(:user) { create(:user) }    
+    let!(:auth_data) { user.create_new_auth_token }
     let(:headers) do
-        {
-            'Accept' => 'application/vnd.taskmanager.v2',
-            'Content-Type' => Mime[:json].to_s
-        }
+      { 
+        'Accept' => 'application/vnd.taskmanager.v2',
+        'Content-type' => Mime[:json].to_s,
+        'access-token' => auth_data['access-token'],
+        'uid' => auth_data['uid'],
+        'client' => auth_data['client']
+      } 
     end
 
-    describe 'POST /sessions' do
+    describe 'POST /auth/sign_in' do
        before do
-         post "/sessions", params: { session: credentials }.to_json, headers: headers   
+         post "/auth/sign_in", params: credentials.to_json, headers: headers   
        end 
 
        context 'when the credentials are correct' do
@@ -22,14 +26,15 @@ RSpec.describe 'Session API', type: :request do
             expect(response).to have_http_status(200)
           end
 
-          it 'returns the json data for the user with auth token' do 
-            user.reload
-            expect(json_body[:data][:attributes][:"auth-token"]).to eq(user.auth_token)
+          it 'returns the authentication data in the headers' do 
+            expect(response.headers).to have_key('access-token')
+            expect(response.headers).to have_key('uid')
+            expect(response.headers).to have_key('client')
           end
        end
 
        context 'when the credentials are incorrect' do
-        let(:credentials) { { email: user.email, password: "invalid_password" } }
+        let(:credentials) { { email: user.email, password: "invalid" } }
            
         it 'returns status code 401' do 
           expect(response).to have_http_status(401)
